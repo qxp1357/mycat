851726d9f3e9784dc63b672cd1d2347452c37192
jaewoook.lee <jaewoook.lee@lgepartner.com>
2023-02-17 18:33:59 +0900

    2.17 완료여부 검사기 완료 이미지 관련 처리가 안되고 있음

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,25 @@ import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-//2.8(수) 각 타일 이동기능 완료, 시작 시 원본 리스트랑 서로 만들어서 비교기능 추가해야함
+//2.8(수) 각 타일 이동기능 완료, 시작 시 원본 리스트랑 서로 만들어서 비교기능 추가해야함 <- 2.17(금) 완료 여부 검사기 완료
+//2.17(금) 이미지 잘라야되는데 내장 API 없어서 다른거 찾아야됨 아오 킹받음
+//처음 이미지 원본 가져옴 더 로딩할 필요 없게
+// final originImg = Image.asset('assets/image/cat_1.jpg');
+// final originImgByteData = originImg.image.toByteData();
 
 
+
+
+
+
+//한칸용 클래스
 class Cell {
   String src;
   bool isEmpty;
   int originIndex;
   Cell(this.src,this.isEmpty,this.originIndex);
 }
-
+// 4*4 퍼즐칸용 배열을 반환하는 만드는 함수
 List makePuzzleList (int idx) {
   List<Cell> res = [];
 
@@ -27,38 +36,51 @@ List makePuzzleList (int idx) {
   return res;
 }
 
+// 두 배열이 서로 동일한지 검사하는 함수
 bool checkComplete(List origin, List current){
-  bool res;
-
+  bool res = true;
+//지금 여기가 다르다가 나오면 안됨 ㅋㅋㅋㅋㅋㅋㅋㅋ
   List originIndexs = [3,3,3,3,3];
   List currentIndexs = [3,3,3,3,3];
 
-  return res = originIndexs == currentIndexs;
+  for(int i = 0; i < origin.length ; i++){
+    if(origin[i] != current[i]){
+      return res = false;
+    }
+  }
+
+  return res;
 }
 
-int emptyIndex = 3;
+//빈자리 const 값
+const int emptyIndex = 3;
 
+//뷰를 뿌리기위한 데이터를 저장하는 A
 List A = makePuzzleList(emptyIndex);
+
+//처음 데이터를 저장해놓을 B
 final List B = A;
+//테스트용
+bool Complete = checkComplete([1,2,3,4,5],[1,2,3,4,5]);
 
-bool Complete = checkComplete([3],[2]);
 
+//메인
 void main() {
   runApp(const MyApp());
 }
-
+//메인에서 실행시킬 스테이트풀 위젯을 상속받는 MyApp
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
-
+//MyApp 에서 실행될 MyApp형태의 State를 상속받는 클래스
 class _MyAppState extends State<MyApp> {
 
 
   void swapData (List data, int currentIndex, int nextIndex){
-
+    //전체 데이터를 받고, 클릭된 인덱스와 이동 가능한 인덱스를 받아서 서로 바꿔주는 함수
     setState(() {
       Cell temp = data[currentIndex];
       data[currentIndex] = data[nextIndex];
@@ -67,6 +89,10 @@ class _MyAppState extends State<MyApp> {
   }
 
   void swapCell (List data, int currentIndex){
+    //클릭되었을 때 직접적으로 작동될 함수
+    //전체 데이터와 클릭된 인덱스를 받아옴
+    //이동 가능한 인덱스를 만들어서 swapData로 넘겨줌
+
     int nextIndex = 0;
     //각 셀이 상하좌우의 움직임에 대해서 계산하면됨
     //상하좌우에 대해서 비어있는지 검사하고 서로 교환
@@ -89,22 +115,16 @@ class _MyAppState extends State<MyApp> {
           isRight && (data[currentIndex + 1].isEmpty == true) ||
           isDown && (data[currentIndex + 4].isEmpty == true) ||
           isLeft && (data[currentIndex - 1].isEmpty == true)) {
-
         swapData(data, currentIndex, nextIndex);
       }
     }
-
-//     setState(() {
-//       Cell temp = data[currentIndex];
-//       data[currentIndex] = data[nextIndex];
-//   data[nextIndex] = temp;
-// });
 }
+
+  // final originImg = Image.asset('assets/image/cat_1.jpg');
+
   @override
   Widget build(BuildContext context) {
-
-
-
+    // print("width:${originImg.width}");
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -141,7 +161,15 @@ class _MyAppState extends State<MyApp> {
         bottomNavigationBar: BottomAppBar(
           child: Container(
               height: 50.0,
-              child: Center(child: Text(Complete ? "같다": "다르다", style: TextStyle(fontSize: 30),))),
+              child: Center(
+                child: Row(
+                  children: [
+                    Text(Image.asset('assets/image/cat_1.jpg',fit: BoxFit.fill).toString()),
+                    Center(child: Text(Complete ? "같다": "다르다", style: TextStyle(fontSize: 30),))
+                  ],
+                ),
+              )
+    ),
         ),
       ),
     );
